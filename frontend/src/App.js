980cb08e88464844a7845e9dd03cd011f962eddf
Santiago Veloso <96207233+santyveloso@users.noreply.github.com>
2025-05-14 21:27:26 +0100

    botoes da landing page levam a pagina de Criar Casa e Juntar via codigo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
-import CreateOrJoin from "./pages/CreateOrJoin";
 import CreateHome from "./pages/CreateHome";
 import JoinHome from "./pages/JoinHome";
 import LoginPage from './pages/LoginPage';
@@ -14,8 +13,7 @@ function App() {
         /*
         //<Route path="/landingpage" element={<LandingPage />} /> 
         */
-          <Route path="/" element={<CreateHome />} />
-        <Route path="/create-or-join" element={<CreateOrJoin />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/create-home" element={<CreateHome />} />
         <Route path="/join-home" element={<JoinHome />} />
         <Route path="/dashboard" element={<Dashboard />} />
